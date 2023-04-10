5fcfcf83c3e7b72edcf0ad0081d40258ed9d1457
tasha-sk666 <tasha.sk666@gmail.com>
2023-04-10 11:51:28 +0300

    fix files

--- src/js/_components.js
+++
@@ -1,7 +0,0 @@
-import "./components/preloader";
-import "./components/theme";
-import "./components/burger";
-import "./components/testimonial-slider";
-import "./components/disableScroll";
-import "./components/enableScroll";
-import "./components/scroll";
--- src/js/_vendor.js
+++
@@ -1,2 +0,0 @@
-/* eslint-disable import/extensions */
-import "./vendor/focus-visible.js";
